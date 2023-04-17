46582c009ea447574dbde39a2b35fb9650918ef1
David Upegui <davidupegui2002@gmail.com>
2023-04-16 19:07:35 -0500

    Changed: Encryption and unencryption functions
    - Now they work with a encryptionRules object.
    - Some documentation added.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Creating the DOM elements
+//Calling the DOM elements ----------------------------------------
 const btnEncrypt = document.getElementById("btn-encrypt");
 const btnUnencrypt = document.getElementById("btn-unencrypt");
 const inpMsg = document.getElementById("inp-msg");
@@ -7,98 +7,97 @@ const divMsgFound = document.getElementById("found");
 const txtEncryptedMsg = document.getElementById("encrypted-msg");
 const btnCopy = document.getElementById("btn-copy");
 
+//Creating initial global values ----------------------------------
+const encryptionRules = {
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
+};
+const unencryptionRules = generateUnencryptionRules();
+
+function generateUnencryptionRules(){
+  const unencryptionRules = {};
+  for(var key in encryptionRules){
+    {
+      unencryptionRules[encryptionRules[key]] = key
+    }
+  }
+  return unencryptionRules
+}
+
+//Encrypt and unencyrpt functions -------------------------------------
 function encryptMessage(msg) {
-  var msgArray = msg.split('');
+  var msgArray = msg.split("");
   var encryptedMsg = "";
   msgArray.forEach((letter) => {
-    switch (letter) {
-      case "a":
-        encryptedMsg += "ai";
-        break;
-      case "e":
-        encryptedMsg += "enter";
-        break;
-      case "i":
-        encryptedMsg += "imes";
-        break;
-      case "o":
-        encryptedMsg += "ober";
-        break;
-      case "u":
-        encryptedMsg += "ufat";
-        break;
-      default:
-        encryptedMsg += letter;
-        break;
-    }
+    encryptedMsg += encryptionRules[letter] ?? letter;
   });
   return encryptedMsg;
 }
-
-function isMessageCorrect(msg){
-  const regexp = /^[a-z\s]+$/
-  return regexp.test(msg);
-}
-
-function unencryptMessage(msg){
+function unencryptMessage(msg) {
   var unencrypted = msg;
-  unencrypted = unencrypted.replaceAll('ai', 'a'); 
-  unencrypted = unencrypted.replaceAll('enter', 'e'); 
-  unencrypted = unencrypted.replaceAll('imes', 'i'); 
-  unencrypted = unencrypted.replaceAll('ober', 'o'); 
-  unencrypted = unencrypted.replaceAll('ufat', 'u'); 
-  return unencrypted
+  for(key in unencryptionRules){
+  unencrypted = unencrypted.replaceAll(key, unencryptionRules[key]);
+  }
+  return unencrypted;
 }
 
-function toggleEncrypt(e) {
+//Functions for the buttons events ---------------------------------------
+function handleToggleEncryptClick(e) {
   var msg;
-  if(e.target.id === 'btn-encrypt'){
+  if (e.target.id === "btn-encrypt") {
     msg = encryptMessage(inpMsg.value);
-  }else{
+  } else {
     msg = unencryptMessage(inpMsg.value);
   }
   showMessage(msg);
 }
+function showMessage(msg) {
+  if (msg === "") {
+    divMsgFound.classList.add("hide");
+    divMsgNotFound.classList.remove("hide");
+    return;
+  }
+  divMsgFound.classList.remove("hide");
+  divMsgNotFound.classList.add("hide");
+  txtEncryptedMsg.innerText = msg;
+}
 
-function handleCopyClick(e){
+//Functions for validation of the message -----------------------------------
+function handleOnInput(e) {
+  const msg = e.target.value;
+  if (!isMessageCorrect(msg)) {
+    disableButton(btnEncrypt);
+    disableButton(btnUnencrypt);
+  } else {
+    enableButton(btnEncrypt);
+    enableButton(btnUnencrypt);
+  }
+}
+function isMessageCorrect(msg) {
+  const regexp = /^[a-z\s]+$/;
+  return regexp.test(msg);
+}
+function disableButton(btn) {
+  btn.disabled = true;
+  btn.classList.add("disabled");
+}
+function enableButton(btn) {
+  btn.disabled = false;
+  btn.classList.remove("disabled");
+}
+
+//Function for the copy-text functionality -----------------------------------
+function handleCopyClick() {
   const copiedText = txtEncryptedMsg.innerText;
   navigator.clipboard.writeText(copiedText);
   alert(`El texto ha sido copiado en el portapapeles.`);
 }
 
-function disableButton(btn){
-  btn.disabled = true;
-  btn.classList.add('disabled');
-}
-
-function enableButton(btn){
-  btn.disabled = false;
-  btn.classList.remove('disabled');
-}
-
-function handleOnInput(e){
-  const msg = e.target.value;
-  if(!isMessageCorrect(msg)){
-    disableButton(btnEncrypt);
-    disableButton(btnUnencrypt);
-  }else{
-    enableButton(btnEncrypt);
-    enableButton(btnUnencrypt);
-  }
-}
-
-function showMessage(msg){
-  if(msg === ""){
-    divMsgFound.classList.add('hide');
-    divMsgNotFound.classList.remove('hide');
-    return
-  }
-  divMsgFound.classList.remove('hide');
-  divMsgNotFound.classList.add('hide');
-  txtEncryptedMsg.innerText = msg;
-}
-
+//Adding the events to the Dom elements. --------------------------------------
+btnEncrypt.onclick = handleToggleEncryptClick;
+btnUnencrypt.onclick = handleToggleEncryptClick;
 inpMsg.oninput = handleOnInput;
-btnEncrypt.onclick = toggleEncrypt;
-btnUnencrypt.onclick = toggleEncrypt;
 btnCopy.onclick = handleCopyClick;
